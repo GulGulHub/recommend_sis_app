9664a775be6e497535fae91d0f0aaad43ab95157
GulGulHub <guelden.ediger@gmx.net>
2023-04-16 20:12:33 +0200

    added show all function

--- static/testFind.js
+++ static/testFind.js
@@ -155,7 +155,7 @@ function onClickValue(){
 }
 
 
-/* Extrafunction to zoom to location*/
+/* Extrafunction to zoom to location - reminder !!!!!! MAKE THIS ONE FUNCTION _ REDUCE LINES!!!!!!!!!!!*/
 
 function zoomToLocation(search_item) {
   search_address = search_item.address;
@@ -180,6 +180,83 @@ function zoomToLocation(search_item) {
             });
   }});
 }
+
+
+/* function that will show all sisters on map and below */
+
+function showAllOnMap() {
+
+  let search_address;
+  let target_div = document.getElementById("Test_JS");
+
+  fetch(`/api/getAll`, { mode: "cors" }) /* makes api call */
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      let all_searches = data.sisters
+      console.log("!!HIER!!",data.sisters)
+      let header_text = document.createElement("h3")
+      target_div.appendChild(header_text)               /* adds the tag as heade-text*/
+      for (const search_item of all_searches) {         /* for item from db: create text and get address for call later*/
+        console.log("!!!", search_item)
+        header_text.innerText = `${search_item.description} : ` 
+        let sis_text = document.createElement("div")
+        sis_text.innerText = search_item.fullname + " - " + search_item.address + " - " + search_item.description + " - " + search_item.contact;
+        sis_text.setAttribute("id","sis_text")
+        sis_text.style.border = "thin solid #200589";
+        sis_text.style.padding = "3px 3px 3px 3px";
+        sis_text.style.width = "fit-content";
+        sis_text.style.cursor = "pointer";
+        sis_text.onclick = function() {
+          zoomToLocation(search_item);    /* I added this function so that if there are too many choices there is an easy zoom*/
+        }
+        target_div.appendChild(sis_text);
+        
+        search_address = search_item.address;
+        const geocodingServiceUrl = "https://geocode-api.arcgis.com/arcgis/rest/services/World/GeocodeServer";
+
+        const params = {
+          address: {
+            address: search_address,
+          },
+        };
+
+        
+
+        esriLocator.addressToLocations(geocodingServiceUrl, params).then((results) => {
+          if (results) {
+            console.log("got result");
+            firstResult = results[0];
+            console.log(firstResult);
+            console.log(firstResult.address);
+
+            view.goTo({
+              target: firstResult.location,
+              zoom: 14,
+            });
+
+            placePoint = firstResult.location;
+            placeInMap(placePoint, search_item.fullname, search_item.description, search_item.address, search_item.contact);
+          } else {
+            console.log("Geocode was not successful");
+            // If you want to provide feedback to the user on the map page:
+            //document.getElementById('addressHelpBlock').innerHTML="Sorry! That search did not work, try again!";
+          }
+
+          
+        });
+      }
+    })
+
+    .catch((err) => console.error(err)); 
+
+
+
+  //prevent refresh
+  return false;
+
+}
+
 
 /* function that creates a point from the address.location above */
 
@@ -207,7 +284,9 @@ function placeInMap(place, fullname, tag, address, contact) {
   };
 
 
-  /* const simpleMarkerSymbol = {
+  /* old marker Symbol
+  
+    const simpleMarkerSymbol = {
     type: "simple-marker",
     style: "diamond",
     color: [255, 105, 180],  // Hotpink
@@ -216,6 +295,8 @@ function placeInMap(place, fullname, tag, address, contact) {
       width: 3
     }
   }; */
+  
+  
   
   /* creates a popup */
 
@@ -263,4 +344,9 @@ function clearMarkers() {
   let target_div = document.getElementById("Test_JS");
   target_div.textContent = '';
 }
+
+
+
+
+
 
